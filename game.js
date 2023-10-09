bfb6453dc9037fe95bba29065bd993c4c68503f4
GennysonJunior <gennysonjunior@gmail.com>
2023-10-09 17:47:58 -0300

    adicionado 3 novas ia's para detectar alfebeto em libras, e adicionado uma função 'finalize()' para parar o loop do jogo.

--- game.js
+++ game.js
@@ -1,18 +1,19 @@
-const URL = "https://teachablemachine.withgoogle.com/models/Vn-uJTd4S/";
+//const URL = "https://teachablemachine.withgoogle.com/models/Vn-uJTd4S/";
+const URL1 = "https://teachablemachine.withgoogle.com/models/yHZpVcXLD/"; // a, c, e, m, n, o ,s, x.
+const URL2 = "https://teachablemachine.withgoogle.com/models/5vPkHjD0V/"; // d, g, i, l, j, q, p.
+const URL3 = "https://teachablemachine.withgoogle.com/models/iieVIMLzz/"; // b, f, r, t, u, v, w, y.
 
-var model, webcam, labelContainer, maxPredictions,
-    letras = ['b', 'c', 'd', 'f', 'g', 'm', 'a', 'e', 'i', 'o', 'u'],
+var model1, model2, model3, webcam, labelContainer, maxPredictions, RAFloop,
+    letras1 = ['a', 'c', 'e', 'm', 'n', 'o' ,'s', 'x'], letras2 = ['d', 'g', 'i', 'l', 'j', 'q', 'p'], letras3 = ['b', 'f', 'r', 't', 'u', 'v', 'w', 'y'],
+    letras = ['a', 'c', 'e', 'm', 'n', 'o', 's' , 'x', 'd', 'g', 'i', 'l', 'j', 'q', 'p', 'b', 'f', 'r', 't', 'u', 'v', 'w', 'y'],
     letra = letras[Math.floor(Math.random() * letras.length)], score = 0, escolher = false, tempo,
     labelScore = document.getElementById("score"),
     labelLetra = document.getElementById("letra"),
     labelTempo = document.getElementById("time"),
     divTela = document.getElementById("tela_fim2");
 
-async function init() {
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
-
-    /* load the model and metadata 
+async function loadModel() {
+     /* load the model and metadata 
     / carregar o modelo e os metadados */
 
     /* Refer to tmImage.loadFromFiles() in the API to support files from a file picker
@@ -23,9 +24,12 @@ async function init() {
 
     /* Note: the pose library adds "tmImage" object to your window (window.tmImage)
     / Nota: a biblioteca de poses adiciona o objeto "tmImage" à sua janela (window.tmImage) */
+    model1 = await tmImage.load(URL1 + "model.json", URL1 + "metadata.json");
+    model2 = await tmImage.load(URL2 + "model.json", URL2 + "metadata.json");
+    model3 = await tmImage.load(URL3 + "model.json", URL3 + "metadata.json");
+}
 
-    model = await tmImage.load(modelURL, metadataURL);
-
+async function init() {
     /* Convenience function to setup a webcam
     / Função de conveniência para configurar uma webcam */
 
@@ -46,12 +50,17 @@ async function init() {
     */
 }
 
+
 async function loop() {
     webcam.update(); /* update the webcam frame / atualize o quadro da webcam */
     await predict();
-    window.requestAnimationFrame(loop);
+    RAFloop = window.requestAnimationFrame(loop);
 }
 
+function finalize() {
+    webcam.stop();
+    window.cancelAnimationFrame(RAFloop);
+}
 /* run the webcam image through the image model 
 / execute a imagem da webcam através do modelo de imagem */
 
@@ -60,7 +69,14 @@ async function predict() {
     /* predict can take in an image, video or canvas html element
     / prever pode incluir um elemento HTML de imagem, vídeo ou tela */
 
-    const prediction = await model.predict(webcam.canvas);
+    let prediction;
+    if (letras1.includes(letra)) {
+        prediction = await model1.predict(webcam.canvas);
+    } else if (letras2.includes(letra)) {
+        prediction = await model2.predict(webcam.canvas);
+    } else if (letras3.includes(letra)) {
+        prediction = await model3.predict(webcam.canvas);
+    }
     /*
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
